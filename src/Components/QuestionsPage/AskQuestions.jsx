bfc0c07958ee3e440d366d54a96a01efe0a62ef1
aakj2010 <aakj2010@gmail.com>
2022-10-02 13:05:38 +0530

    ask

--- src/Components/QuestionsPage/AskQuestions.jsx
+++ src/Components/QuestionsPage/AskQuestions.jsx
@@ -24,7 +24,7 @@ function AskQuestions() {
             if (values.question === "") {
                 errors.question = "Please Enter Title";
             }
-            if(values.answer ===''){
+            if (values.answer === '') {
                 errors.answer = "Please Enter Description"
             }
 
@@ -47,61 +47,67 @@ function AskQuestions() {
             <div className='container mt-5 mb-5'>
                 <h3>Ask a Public Questions</h3>
             </div>
-            <form onSubmit={formik.handleSubmit}>
-                <div className='container col-lg-6 border ask shadow-sm p-3 mb-5 bg-body rounded'>
-                    <div className="mt-1">
-                        <label className='fw-bold fs-2'>Title</label><br />
-                        <small>Be specific and imagine you’re asking a question to another person</small>
-                        <input className={`form-control ${formik.errors.question ? `input-error` : ``}`}
-                            type={"text"}
-                            value={formik.values.question}
-                            onChange={formik.handleChange}
-                            name="question" />
-                        <span style={{ color: 'red' }}>{formik.errors.question}</span>
-                    </div>
-                    <div className="mt-3">
-                        <label className='fw bold'>Body</label><br />
-                        <small>Include all the information someone would need to answer your question</small>
 
-                        <div class="btn-group border " role="group" aria-label="First group">
-                            <button type="button" class="btn btn-link text-secondary"> Links</button>
-                            <button type="button" class="btn btn-link text-secondary"> Images</button>
-                            <button type="button" class="btn btn-link text-secondary"> Styling / Headers</button>
-                            <button type="button" class="btn btn-link text-secondary"> Lists</button>
-                            <button type="button" class="btn btn-link text-secondary"> Code</button>
-                            <button type="button" class="btn btn-link text-secondary"> Blockqoutes</button>
-                            <button type="button" class="btn btn-link text-secondary"> Html</button>
-                            <button type="button" class="btn btn-link text-secondary"> Tables</button>
-                            <button type="button" class="btn btn-link "> More</button>
-                        </div>
-                        <div class="mb-3">
-                            <textarea className={`form-control ${formik.errors.answer ? `input-error` : ``}`}
-                                name="answer" id="exampleFormControlTextarea1"
-                                rows="4"
-                                value={formik.values.answer}
-                                onChange={formik.handleChange}>
-                            </textarea>
-                            <span style={{ color: 'red' }}>{formik.errors.answer}</span>
+            <div className=' askform '>
+
+                <form onSubmit={formik.handleSubmit}>
+                    <div className='container col-lg-6 border askform shadow-sm p-3 mb-5 bg-body rounded'>
+                        <div className="mt-1">
+                            <label className='fw-bold fs-2'>Title</label><br />
+                            <small>Be specific and imagine you’re asking a question to another person</small>
+                            <input className={`form-control ${formik.errors.question ? `input-error` : ``}`}
+                                type={"text"}
+                                value={formik.values.question}
+                                onChange={formik.handleChange}
+                                name="question" />
+                            <span style={{ color: 'red' }}>{formik.errors.question}</span>
                         </div>
                         <div className="mt-3">
-                            <label className='fw-bold fs-2'>Tags</label><br />
-                            <small>Add up to 5 tags to describe what your question is about</small>
-                            <input className={`form-control ${formik.errors.tags ? `input-error` : ``}`}
-                                type={"text"}
-                                value={formik.values.tags}
-                                onChange={formik.handleChange}
-                                name="tags"
-                                placeholder='eg. c++,java,python' />
-                            <span style={{ color: 'red' }}>{formik.errors.tags}</span>
+                            <label className='fw bold'>Body</label><br />
+                            <small>Include all the information someone would need to answer your question</small>
+
+                            <div class="btn-group border " role="group" aria-label="First group">
+                                <button type="button" class="btn btn-link text-secondary"> Links</button>
+                                <button type="button" class="btn btn-link text-secondary"> Images</button>
+                                <button type="button" class="btn btn-link text-secondary"> Styling / Headers</button>
+                                <button type="button" class="btn btn-link text-secondary"> Lists</button>
+                                <button type="button" class="btn btn-link text-secondary"> Code</button>
+                                <button type="button" class="btn btn-link text-secondary"> Blockqoutes</button>
+                                <button type="button" class="btn btn-link text-secondary"> Html</button>
+                                <button type="button" class="btn btn-link text-secondary"> Tables</button>
+                                <button type="button" class="btn btn-link "> More</button>
+                            </div>
+                            <div class="mb-3">
+                                <textarea className={`form-control ${formik.errors.answer ? `input-error` : ``}`}
+                                    name="answer" id="exampleFormControlTextarea1"
+                                    rows="4"
+                                    value={formik.values.answer}
+                                    onChange={formik.handleChange}>
+                                </textarea>
+                                <span style={{ color: 'red' }}>{formik.errors.answer}</span>
+                            </div>
+                            <div className="mt-3">
+                                <label className='fw-bold fs-2'>Tags</label><br />
+                                <small>Add up to 5 tags to describe what your question is about</small>
+                                <input className={`form-control ${formik.errors.tags ? `input-error` : ``}`}
+                                    type={"text"}
+                                    value={formik.values.tags}
+                                    onChange={formik.handleChange}
+                                    name="tags"
+                                    placeholder='eg. c++,java,python' />
+                                <span style={{ color: 'red' }}>{formik.errors.tags}</span>
+                            </div>
+
+                        </div>
+                        <div className='mt-3'>
+                            <button class="btn btn-primary" type={"submit"} value="Submit">Review Question</button>
                         </div>
 
                     </div>
-                    <div className='mt-3'>
-                        <button class="btn btn-primary" type={"submit"} value="Submit">Review Question</button>
-                    </div>
+                </form>
 
-                </div>
-            </form>
+            </div>
+
 
 
         </>
