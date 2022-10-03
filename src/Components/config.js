9b192e4d528d0d27915aaa274b85e4e4543527f7
aakj2010 <aakj2010@gmail.com>
2022-10-03 13:40:18 +0530

    final commit 3/2

--- src/Components/config.js
+++ src/Components/config.js
@@ -1,4 +1,4 @@
 export const env ={
-    api: "http://localhost:3100"
-    // api : "https://stackofbd.herokuapp.com"
+    // api: "http://localhost:3100"
+    api : "https://stackofbd.herokuapp.com"
 }
