11cab92a0948bc1de09e29e5f61fdf1ac5bfcb3c
aakj2010 <aakj2010@gmail.com>
2023-01-03 13:32:15 +0530

    backend con

--- src/Components/ProtectedRoute.js
+++ src/Components/ProtectedRoute.js
@@ -1,37 +1,8 @@
-// import React from 'react'
-// import { Navigate, Outlet, Redirect, Route, useNavigate } from "react-router-dom";
-// import LoginPage from './LoginPage';
-
-
-// const authUser = () => {
-//   const user = { login: true };
-
-//   return user && user.login
-// }
-
-// export default function ProtectedRoute(props) {
-
-
-//   // const navigate = useNavigate()
-//   // const user = window.localStorage.getItem("app-token");
-//   // if (!user) {
-//   //   return navigate("/");
-//   // } else {
-//   //   return <Route {...props} />;
-//   // }
-
-//   isAuth ? <Outlet /> : <Navigate to='/' />
-
-// }
-
-
 import React from 'react'
 import { Navigate, Outlet } from "react-router-dom";
-import LoginPage from './LoginPage';
 
 
 const authUser = () => {
-  // const user = { login: true };
   const user = window.localStorage.getItem("app-token");
 
   return user
